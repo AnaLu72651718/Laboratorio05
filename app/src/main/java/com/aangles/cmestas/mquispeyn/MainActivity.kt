4aa50941546702ca0da0317b9969864cd77c6047
Ana Lucia <aangles@unsa.edu.pe>
2022-06-08 23:24:30 -0500

    Initial commit

---
+++ app/src/main/java/com/aangles/cmestas/mquispeyn/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.aangles.cmestas.mquispeyn
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.aangles.cmestas.mquispeyn.ui.theme.Laboratorio05Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Laboratorio05Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Laboratorio05Theme {
+        Greeting("Android")
+    }
+}
